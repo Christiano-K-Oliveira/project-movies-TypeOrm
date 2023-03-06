38b62b32a6974a1c1c47650ea395206cb6133c1b
Christiano-K-Oliveira <kayky.20oliveira@gmail.com>
2023-03-06 20:26:56 -0300

    fix: corrigido a verificação para verificar se o page e o perPage não são números

--- src/services/movies/listMovies.service.ts
+++ src/services/movies/listMovies.service.ts
@@ -7,8 +7,8 @@ import { allMoviesSchema } from '../../schemas/movies.schemas'
 const listMoviesService = async (payload: any): Promise<iResultGetMovies> => {
     const movieRepo: Repository<Movie> = AppDataSource.getRepository(Movie)
 
-    const page: number = payload.page === undefined || parseInt(payload.page) <= 1 ? 1 : parseInt(payload.page)
-    const perPage: number = payload.perPage === undefined || parseInt(payload.perPage) <= 0 || parseInt(payload.perPage) > 5 ? 5 : parseInt(payload.perPage)
+    const page: number = payload.page === undefined || isNaN(payload.page) || parseInt(payload.page) <= 1 ? 1 : parseInt(payload.page)
+    const perPage: number = payload.perPage === undefined || isNaN(payload.perPage) || parseInt(payload.perPage) <= 0 || parseInt(payload.perPage) > 5 ? 5 : parseInt(payload.perPage)
     const sort: string = payload.sort === undefined || payload.sort.toLowerCase() !== 'price' && payload.sort.toLowerCase() !== 'duration' ? 'id' : payload.sort
     const order: string = payload.order === undefined || payload.sort === undefined || payload.order.toLowerCase() !== 'asc' && payload.order.toLowerCase() !== 'desc' ? 'ASC' : payload.order
 
